e47217b1ef097ce925a12a2ca399146403781edc
Patrick Kernke <patrick@kernke.net>
2015-02-11 16:02:38 +0100

    Wobble-Effekt für Kontaktbutton

--- resources/assets/js/coming.js
+++ resources/assets/js/coming.js
@@ -7,6 +7,13 @@ $( document ).ready(function() {
 		overlayContact.addClass('open');
 	});
 
+	var interval = setInterval(function() {
+		contactOpenBtn.addClass('wobble');
+		setTimeout(function() {
+			contactOpenBtn.removeClass('wobble');
+		}, 2500);
+	}, 4000);
+
 	var contactHideBtn = $('#contactHideBtn');
 	contactHideBtn.on('click', function() {
 		overlayContact.removeClass('open');
